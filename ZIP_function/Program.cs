7e2ef01698c0c36dbc4355daa7b3ed735d734d5c
Wojciech Klepacki <wklepa@outlook.com>
2022-07-21 15:44:03 +1000

    Add project files.

---
+++ ZIP_function/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Three source arrays.
+        var array1 = new int[] { 1, 2, 3, 4, 5 };
+        var array2 = new int[] { 6, 7, 8, 9, 10 };
+        var array3 = new char[] {'a', 'b', 'c', 'd', 'e'};
+
+        // Add elements at each position together.
+        var zipped = array1.Zip(array2, (a, b) => new {a, b}).Zip(array3, (c, d) => new {AA = c.a, BB = c.b, CC = d});
+        // Zip method uses lambda expression after '=>'
+
+        // Look at results.
+        foreach (var value in zipped)
+        {
+            // Access the elements in the zipped lists
+            string A = value.AA.ToString();
+            string B = value.BB.ToString();
+            char C = value.CC;
+            Console.WriteLine("{0}, {1}, {2}", A, B, C);
+        }
+
+    }
+}
